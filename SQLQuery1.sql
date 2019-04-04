775efe7a761f2ad3287f12f061275c4aea8e4646
Jiayao Yang <jy625423987@gmail.com>
2019-04-04 15:51:56 +1100

    fix the database bug

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[CrimeRate](SuburbSuburbName,Rate,OffenceCount,Totpopulation)
+VALUES('Acheron','0.0234','4','171')
--- gosafe-back/Models/Model1.edmx.sql
+++ gosafe-back/Models/Model1.edmx.sql
@@ -2,13 +2,13 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 04/01/2019 21:14:02
--- Generated from EDMX file: C:\Users\czcz2\IEProject\gosafe-back\gosafe-back\Models\Model1.edmx
+-- Date Created: 04/04/2019 15:47:15
+-- Generated from EDMX file: C:\Users\Jennifer\Desktop\IEproject\gosafe-back\gosafe-back\Models\Model1.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
 GO
-USE [Gosafe-Database];
+USE [Database1];
 GO
 IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
 GO
@@ -17,50 +17,77 @@ GO
 -- Dropping existing FOREIGN KEY constraints
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[FK_EmergencyContactUserEmergency]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[UserEmergency] DROP CONSTRAINT [FK_EmergencyContactUserEmergency];
+GO
+IF OBJECT_ID(N'[dbo].[FK_UserProfileUserEmergency]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[UserEmergency] DROP CONSTRAINT [FK_UserProfileUserEmergency];
+GO
+IF OBJECT_ID(N'[dbo].[FK_SuburbCrimeRate]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Suburb] DROP CONSTRAINT [FK_SuburbCrimeRate];
+GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[UserProfile]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UserProfile];
+GO
+IF OBJECT_ID(N'[dbo].[UserEmergency]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UserEmergency];
+GO
+IF OBJECT_ID(N'[dbo].[EmergencyContact]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[EmergencyContact];
+GO
+IF OBJECT_ID(N'[dbo].[Suburb]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Suburb];
+GO
+IF OBJECT_ID(N'[dbo].[CrimeRate]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[CrimeRate];
+GO
 
 -- --------------------------------------------------
 -- Creating all tables
 -- --------------------------------------------------
 
--- Creating table 'UserProfileSet'
-CREATE TABLE [dbo].[UserProfileSet] (
-    [UserID] int IDENTITY(1,1) NOT NULL,
+-- Creating table 'UserProfile'
+CREATE TABLE [dbo].[UserProfile] (
+    [Id] int IDENTITY(1,1) NOT NULL,
     [address] nvarchar(max)  NOT NULL,
-    [gender] nvarchar(max)  NOT NULL
+    [gender] nvarchar(max)  NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
 );
 GO
 
--- Creating table 'UserEmergencySet'
-CREATE TABLE [dbo].[UserEmergencySet] (
-    [UserProfileUserID] int  NOT NULL,
-    [EmergencyContactContactPhone] int  NOT NULL
+-- Creating table 'UserEmergency'
+CREATE TABLE [dbo].[UserEmergency] (
+    [EmergencyContactPhone] int IDENTITY(1,1) NOT NULL,
+    [UserProfileId] int  NOT NULL
 );
 GO
 
--- Creating table 'EmergencyContactSet'
-CREATE TABLE [dbo].[EmergencyContactSet] (
-    [ContactPhone] int IDENTITY(1,1) NOT NULL,
-    [ContactName] nvarchar(max)  NOT NULL
+-- Creating table 'EmergencyContact'
+CREATE TABLE [dbo].[EmergencyContact] (
+    [Phone] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
 );
 GO
 
--- Creating table 'SuburbSet'
-CREATE TABLE [dbo].[SuburbSet] (
-    [SuburbName] int IDENTITY(1,1) NOT NULL,
-    [Postcode] nvarchar(max)  NOT NULL,
+-- Creating table 'Suburb'
+CREATE TABLE [dbo].[Suburb] (
+    [SuburbName] nvarchar(max)  NOT NULL,
+    [Postcode] nchar(4000)  NOT NULL,
     [Boundary] nvarchar(max)  NOT NULL
 );
 GO
 
--- Creating table 'CrimeRateSet'
-CREATE TABLE [dbo].[CrimeRateSet] (
-    [SuburbName] int  NOT NULL,
-    [Rate] nvarchar(max)  NOT NULL
+-- Creating table 'CrimeRate'
+CREATE TABLE [dbo].[CrimeRate] (
+    [SuburbSuburbName] nvarchar(max)  NOT NULL,
+    [Rate] nchar(4000)  NOT NULL,
+    [OffenceCount] nvarchar(max)  NOT NULL,
+    [Totpopulation] nvarchar(max)  NOT NULL
 );
 GO
 
@@ -68,69 +95,69 @@ GO
 -- Creating all PRIMARY KEY constraints
 -- --------------------------------------------------
 
--- Creating primary key on [UserID] in table 'UserProfileSet'
-ALTER TABLE [dbo].[UserProfileSet]
-ADD CONSTRAINT [PK_UserProfileSet]
-    PRIMARY KEY CLUSTERED ([UserID] ASC);
+-- Creating primary key on [Id] in table 'UserProfile'
+ALTER TABLE [dbo].[UserProfile]
+ADD CONSTRAINT [PK_UserProfile]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
--- Creating primary key on [UserProfileUserID], [EmergencyContactContactPhone] in table 'UserEmergencySet'
-ALTER TABLE [dbo].[UserEmergencySet]
-ADD CONSTRAINT [PK_UserEmergencySet]
-    PRIMARY KEY CLUSTERED ([UserProfileUserID], [EmergencyContactContactPhone] ASC);
+-- Creating primary key on [EmergencyContactPhone], [UserProfileId] in table 'UserEmergency'
+ALTER TABLE [dbo].[UserEmergency]
+ADD CONSTRAINT [PK_UserEmergency]
+    PRIMARY KEY CLUSTERED ([EmergencyContactPhone], [UserProfileId] ASC);
 GO
 
--- Creating primary key on [ContactPhone] in table 'EmergencyContactSet'
-ALTER TABLE [dbo].[EmergencyContactSet]
-ADD CONSTRAINT [PK_EmergencyContactSet]
-    PRIMARY KEY CLUSTERED ([ContactPhone] ASC);
+-- Creating primary key on [Phone] in table 'EmergencyContact'
+ALTER TABLE [dbo].[EmergencyContact]
+ADD CONSTRAINT [PK_EmergencyContact]
+    PRIMARY KEY CLUSTERED ([Phone] ASC);
 GO
 
--- Creating primary key on [SuburbName] in table 'SuburbSet'
-ALTER TABLE [dbo].[SuburbSet]
-ADD CONSTRAINT [PK_SuburbSet]
+-- Creating primary key on [SuburbName] in table 'Suburb'
+ALTER TABLE [dbo].[Suburb]
+ADD CONSTRAINT [PK_Suburb]
     PRIMARY KEY CLUSTERED ([SuburbName] ASC);
 GO
 
--- Creating primary key on [SuburbName] in table 'CrimeRateSet'
-ALTER TABLE [dbo].[CrimeRateSet]
-ADD CONSTRAINT [PK_CrimeRateSet]
-    PRIMARY KEY CLUSTERED ([SuburbName] ASC);
+-- Creating primary key on [SuburbSuburbName] in table 'CrimeRate'
+ALTER TABLE [dbo].[CrimeRate]
+ADD CONSTRAINT [PK_CrimeRate]
+    PRIMARY KEY CLUSTERED ([SuburbSuburbName] ASC);
 GO
 
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
 
--- Creating foreign key on [UserProfileUserID] in table 'UserEmergencySet'
-ALTER TABLE [dbo].[UserEmergencySet]
-ADD CONSTRAINT [FK_UserProfileUserEmergency]
-    FOREIGN KEY ([UserProfileUserID])
-    REFERENCES [dbo].[UserProfileSet]
-        ([UserID])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating foreign key on [EmergencyContactContactPhone] in table 'UserEmergencySet'
-ALTER TABLE [dbo].[UserEmergencySet]
+-- Creating foreign key on [EmergencyContactPhone] in table 'UserEmergency'
+ALTER TABLE [dbo].[UserEmergency]
 ADD CONSTRAINT [FK_EmergencyContactUserEmergency]
-    FOREIGN KEY ([EmergencyContactContactPhone])
-    REFERENCES [dbo].[EmergencyContactSet]
-        ([ContactPhone])
+    FOREIGN KEY ([EmergencyContactPhone])
+    REFERENCES [dbo].[EmergencyContact]
+        ([Phone])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_EmergencyContactUserEmergency'
-CREATE INDEX [IX_FK_EmergencyContactUserEmergency]
-ON [dbo].[UserEmergencySet]
-    ([EmergencyContactContactPhone]);
+-- Creating foreign key on [UserProfileId] in table 'UserEmergency'
+ALTER TABLE [dbo].[UserEmergency]
+ADD CONSTRAINT [FK_UserProfileUserEmergency]
+    FOREIGN KEY ([UserProfileId])
+    REFERENCES [dbo].[UserProfile]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating foreign key on [SuburbName] in table 'CrimeRateSet'
-ALTER TABLE [dbo].[CrimeRateSet]
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserProfileUserEmergency'
+CREATE INDEX [IX_FK_UserProfileUserEmergency]
+ON [dbo].[UserEmergency]
+    ([UserProfileId]);
+GO
+
+-- Creating foreign key on [SuburbSuburbName] in table 'CrimeRate'
+ALTER TABLE [dbo].[CrimeRate]
 ADD CONSTRAINT [FK_SuburbCrimeRate]
-    FOREIGN KEY ([SuburbName])
-    REFERENCES [dbo].[SuburbSet]
+    FOREIGN KEY ([SuburbSuburbName])
+    REFERENCES [dbo].[Suburb]
         ([SuburbName])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
