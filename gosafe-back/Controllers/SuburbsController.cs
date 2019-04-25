f6e69fe158d889a84ddaf641435ba2d2647322f7
nobodyczcz <jianshen858@gmail.com>
2019-04-25 11:57:15 +1000

    bug fix

--- gosafe-back/Controllers/SuburbsController.cs
+++ gosafe-back/Controllers/SuburbsController.cs
@@ -5,7 +5,9 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
+using gosafe_back.Results;
+using System.Threading.Tasks;
 using gosafe_back.Models;
 using Newtonsoft.Json;
 using Twilio;
@@ -26,20 +28,21 @@ namespace gosafe_back.Controllers
     }
 
     [RoutePrefix("api/Suburbs")]
-    public class SuburbsController : Controller
+    public class SuburbsController : ApiController
     {
         private Model1Container db = new Model1Container();
 
-        // GET: Suburbs
-        public ActionResult Index()
+        // post: api/Suburbs/Index
+        [Route("Index")]
+        public IHttpActionResult Index()
         {
             var suburb = db.Suburb.Count();
-            return View();
+            return Ok(suburb);
         }
 
-        // GET: Suburbs/Details
-        [HttpPost]
-        public ActionResult Details(List<String> suburbs)
+        // POST: api/Suburbs/Details
+        [Route("Details")]
+        public IHttpActionResult Details(List<String> suburbs)
         {
             System.Diagnostics.Debug.WriteLine("debug:");
             System.Diagnostics.Debug.WriteLine(suburbs);
@@ -75,9 +78,8 @@ namespace gosafe_back.Controllers
             //Suburb suburb = db.Suburb.Find(id);
 
         }
-
-        [HttpPost]
-        public ActionResult Message(List<String> message)
+        [Route("Message")]
+        public IHttpActionResult Message(List<String> message)
         {
             string accountSid = "AC90b329101b6566d76485239d50f6ea00";
             string authToken = "be4151a4e580bf43eadb860c61217e2b";
@@ -106,89 +108,12 @@ namespace gosafe_back.Controllers
 
         }
 
-        // GET: Suburbs/Create
-        public ActionResult Create()
-        {
-            ViewBag.SuburbName = new SelectList(db.CrimeRate, "SuburbSuburbName", "Rate");
-            return View();
-        }
 
-        // POST: Suburbs/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "SuburbName,Postcode,Boundary1,Boundary2")] Suburb suburb)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Suburb.Add(suburb);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
 
-            ViewBag.SuburbName = new SelectList(db.CrimeRate, "SuburbSuburbName", "Rate", suburb.SuburbName);
-            return View(suburb);
-        }
 
-        // GET: Suburbs/Edit/5
-        public ActionResult Edit(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Suburb suburb = db.Suburb.Find(id);
-            if (suburb == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.SuburbName = new SelectList(db.CrimeRate, "SuburbSuburbName", "Rate", suburb.SuburbName);
-            return View(suburb);
-        }
 
-        // POST: Suburbs/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "SuburbName,Postcode,Boundary1,Boundary2")] Suburb suburb)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(suburb).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.SuburbName = new SelectList(db.CrimeRate, "SuburbSuburbName", "Rate", suburb.SuburbName);
-            return View(suburb);
-        }
 
-        // GET: Suburbs/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Suburb suburb = db.Suburb.Find(id);
-            if (suburb == null)
-            {
-                return HttpNotFound();
-            }
-            return View(suburb);
-        }
 
-        // POST: Suburbs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            Suburb suburb = db.Suburb.Find(id);
-            db.Suburb.Remove(suburb);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
